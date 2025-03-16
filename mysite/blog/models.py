6f2cb3a22c76ab04f80243d6a9745854b929d37e
marisamak <glebushkapavl2009q@gmail.com>
2025-03-16 15:56:23 +0300

    add

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Status(models.TextChoices):
-    DRAFT = 'DF', 'Draft'
-    PUBLISHED = 'PB', 'Published'
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
 
@@ -23,11 +28,16 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices,
                               default=Status.DRAFT)
 
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
+
+
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish'])
         ]
+
 
     def __str__(self):
         return self.title
